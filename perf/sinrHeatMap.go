947467734016ea15f05f1b1772387539e9c2f5e0
VinayGupta23 <vinay.23.gupta@outlook.com>
2016-07-14 09:42:23 +0530

    Bug fix for heat map

--- perf/sinrHeatMap.go
+++ perf/sinrHeatMap.go
@@ -3,12 +3,17 @@ package perf
 import (
 	"FrontEnd_WebTools/model"
 	"FrontEnd_WebTools/service"
+	"math"
 )
 
 func SinrHeatMap(sc *model.Scenario, hexMap *service.HexMap, p *Params) map[string]interface{} {
 	preSinrVals := make([]float64, len(sc.Users()))
 	postSinrVals := make([]float64, len(sc.Users()))
 	for i := 0; i < len(sc.Users()); i++ {
+		if hexMap.FindContainingHex(sc.Users()[i].X(), sc.Users()[i].Y()) == nil {
+			preSinrVals[i] = math.NaN()
+			postSinrVals[i] = math.NaN()
+		}
 		temp := SinrProfile(sc, hexMap, sc.Users()[i].ID(), 0, p)
 		preSinrVals[i] = temp["pre"].(float64)
 		postSinrVals[i] = temp["post"].(float64)
