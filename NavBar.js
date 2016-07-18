8c6cb98e5c166fd9aad129cb1fb2687e60ebaa80
ap1729 <ee14b068@smail.iitm.ac.in>
2016-07-18 08:53:49 +0530

    Error msg frontend

--- NavBar.js
+++ NavBar.js
@@ -1,16 +1,13 @@
  document.getElementById("NavBar").innerHTML =
 // '<a class="navbar-brand" href="LevelBackend.html">Omni</a>'+
 '<a class="navbar-brand" href="sectoring.html">Sectoring</a>'+
-'<a class="navbar-brand" href="emergency.html">Emergency</a>'+
+//'<a class="navbar-brand" href="emergency.html">Emergency</a>'+ //Commented as Emergency is in Sectoring
  
-      '<ul class="nav navbar-nav navbar-right">'+
+    
      /*  ' <li class="active">'+
           '<a href="#">Reload</a>'+
         '</li>'+*/	
-        '<li>'+
-          '<a href="index1.html">Home</a>'+
-        '</li>'+
-      '</ul>' +
+          '<a class="navbar-brand  navbar-right" href="index1.html">Home</a>'+
       '<a class="navbar-brand  navbar-right" href="Mosssaic.html">About MOSSSAIC</a>';
 
 
