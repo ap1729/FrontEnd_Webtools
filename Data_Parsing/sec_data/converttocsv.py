3a282ee13835e23b073fc8a9afd8416ab490abcf
ap1729 <ee14b068@smail.iitm.ac.in>
2016-09-19 18:02:03 +0530

    CSV Loss files added

--- Data_Parsing/sec_data/converttocsv.py
+++ Data_Parsing/sec_data/converttocsv.py
@@ -1,34 +1,37 @@
 import sys,os,re,csv
-from numpy import *
-file =open('extracted_file1.txt','r')#extracted_file.txt
-data= file.read()
-data1=data.split()
+import numpy as np
+file =open('iteration1.txt','r')#extracted_file.txt
+#data= file.read()
+#data1=data.split()
 #print data1[:-1]
 count=-1
-elem=[]
+w, h =  228,11400
+elem = [[0 for x in range(w)] for y in range(h)]
 elem1=[]
 ''''
 for i in range(0,228): #76 is number of basestations, 228 for sectoring
  elem1.append(str(i)) #"A"+str(i) 
 ''' 
-with open('sectorloss.csv', 'w') as csvfile:
+with open('sectorloss1.csv', 'w') as csvfile:
+      while count<227:   
+       line=file.readline()
+       linedata=line.split()
+       print len(linedata)
        writer = csv.writer(csvfile)
        #writer.writerow(elem1)
-       for num in data1:
+       count+=1
+       for i in range(0,11400):
+         #print i,count
+         elem[i][count]=linedata[i]
+#       print elem[227][count],linedata[227]
+      count=0
+      while count<11400:
+         elem1=[]
+         for i in range(0,228):
+          elem1.append(elem[count][i])
          count+=1
-         if count%228==0 and count>0: #76 is number of basestations, 228 for sectoring
-           writer.writerow(elem)
-           elem=[]
-         try:
-        # if this succeeds it is float
-           elem.append(float(num))
-         except ValueError:
-           try: 
-             elem.append(float(num[1:]))#print float(num[1:]) #elem.append(float(num[1:])) 
-           #except ValueError:
-           except ValueError:
-             print ""  
-       writer.writerow(elem1)
+         writer.writerow(elem1)
+       
       
 '''
 find=re.search('(-\d+\.*\d* \;*)+',data)
